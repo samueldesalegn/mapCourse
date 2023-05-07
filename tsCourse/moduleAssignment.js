08872c5628706bca276438aaed74735e51e57495
samueldesalegn <samueldesalegnt@gmail.com>
2023-05-06 20:17:30 -0500

    Lab3 assignment submission

---
+++ tsCourse/moduleAssignment.js
@@ -0,0 +1,59 @@
+const shoppingCart = (function() {
+	let basket = [];
+	return {
+	upsertItem(item) {
+		// for (const elem of basket) {
+		if (!basket.includes(item)) {
+			basket.push(item);
+		} else {
+			for (const elem of basket) {
+				if (elem.id === item.id){
+				basket[elem] = item;
+			}
+		}
+		}
+		
+	
+	},
+	getItemsCount() {
+		console.log(basket.length);
+	// console.log("counter value prior to reset: " + counter);
+	// counter = 0;
+	}, 
+	getTotalPrice() {
+		let total = 0;
+		for (const elem of basket) {
+			total += elem.product.price * elem.count;
+		}
+		return total;
+	},
+	removeItemById(id) {
+		let index = basket.findIndex(x => x.id === id);
+		basket.splice(index, 1);
+	}
+	};
+})();
+
+const item1 = { id: 0, product: { id: 1, name: 'Coffee', description: 'Coffee Grounds from Ethiopia', price: 9 }, count: 1 }
+const item2 = { id: 1, product: { id: 2, name: 'Tea', description: 'Oonlong Tea from China', price: 10 }, count: 5 }
+const item3 = { id: 2, product: { id: 3, name: 'Bottled Water', description: 'Bottled Water from US', price: 2 }, count: 30 }
+
+shoppingCart.upsertItem(item1);
+shoppingCart.upsertItem(item2);
+shoppingCart.upsertItem(item3);
+console.log(shoppingCart.getTotalPrice()); //Expected Result: 119
+
+item3.product.name = 'Himilayan Water';
+item3.product.price = 10;
+shoppingCart.upsertItem(item3);
+
+console.log(shoppingCart.basket);
+
+console.log(shoppingCart.getItemsCount()); //Expected Result: 3
+console.log(shoppingCart.getTotalPrice()); //Expected Result: 359
+shoppingCart.removeItemById(1);
+console.log(shoppingCart.getItemsCount()); //Expected Result: 2
+console.log(shoppingCart.getTotalPrice()); //Expected Result: 309
+
+
+	
---
+++ tsCourse/observerAssignment.js
@@ -0,0 +1,44 @@
+class Subject {
+	
+	constructor() {
+		this.observer = {}
+	}
+
+	
+on(event, fn) {
+	if (this.observer[event]) {
+		this.observer[event].push(fn)
+	}else {
+
+		this.observer[event] = [fn];
+	}
+	
+}
+
+emit(event, msg){
+	this.observer[event].forEach(ob=> ob(msg))
+}
+
+
+	
+   
+}
+ 
+const subject = new Subject();
+subject.on('eat', console.log); // register an observer
+subject.on('study', console.log); // register an observer
+
+function foo(msg) {
+    console.log('foo: ' + msg);
+}
+subject.on('eat', foo);
+subject.on('study', foo);
+
+subject.emit('eat', 'Corn');
+//output for Line above: subject.emit('eat', 'Corn');
+// Corn
+// foo: Corn
+subject.emit('study', 'cs445');
+//output for Line above: subject.emit('study', 'cs445');
+// cs445
+// foo: cs445
